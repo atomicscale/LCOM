64b0a4a1b05207e60fc8fd2f7c847fdd7b6688d1
up201207046 <up201207046@7d022d26-2eca-423b-9939-685a0713be1c>
2014-11-17 15:17:43 +0000

    square and line with ESC exit
    git-svn-id: https://svn.fe.up.pt/repos/lcom1415-t3g07@79 7d022d26-2eca-423b-9939-685a0713be1c

--- lab5/test5.c
+++ lab5/test5.c
@@ -1,80 +1,126 @@
 #include "video_gr.h"
+#include <minix/syslib.h>
+#include <minix/drivers.h>
+#include <minix/com.h>
+#include "keyboard.h"
+#include "i8042.h"
+#include "timer.h"
 
 void *test_init(unsigned short mode, unsigned short delay) {
 	vg_init(mode);
 	vg_exit();
 }
 
-int test_square(unsigned short x, unsigned short y,short size, unsigned long color) {
+int test_square(unsigned short x, unsigned short y, short size,
+		unsigned long color) {
 
-	if (size <= 0){
-		printf ("Wrong argument: Size");
+	if (size <= 0) {
+		printf("Wrong argument: Size");
 		return 1;
 	}
 
-	else{
+	else {
 		vg_init(0x105);
-		draw_rectangle(x, y, x+size, y+size, color);
-		sleep(5);
+		draw_rectangle(x, y, x + size, y + size, color);
+
+		int ipc_status;
+		message msg;
+		int validation = 0;
+		int receive;
+
+		int irq_set = kbd_subscribe();
+
+		while (!validation) {
+			receive = driver_receive(ANY, &msg, &ipc_status);
+			if (receive != 0) {
+				printf("driver_receive failed with: %d", receive);
+				continue;
+			}
+
+			if (is_ipc_notify(ipc_status)) {
+				switch (_ENDPOINT_P(msg.m_source)) {
+				case HARDWARE:
+					if (msg.NOTIFY_ARG & irq_set) {
+						validation = kbd_handler_c();
+					}
+					break;
+				default:
+					break;
+				}
+			} else {
+			}
+		}
+		kbd_unsubscribe();
 		vg_exit();
 		return 0;
 	}
 }
 
-int test_line(unsigned short xi, unsigned short yi, unsigned short xf, unsigned short yf, char color) {
+int test_line(unsigned short xi, unsigned short yi, unsigned short xf,
+		unsigned short yf, char color) {
 	vg_init(0x105);
-	/* Midpoint Line algorithm */
-	int dx,dy,d,incry,incre,incrne,slopegt1=0;
-		dx=abs(xi-xf);dy=abs(yi-yf);
-		if(dy>dx)
-		{
-			SWAP(xi,yi);
-			SWAP(xf,yf);
-			SWAP(dx,dy);
-			slopegt1=1;
+
+	int dx = abs(xf - xi), sx = xi < xf ? 1 : -1;
+	int dy = abs(yf - yi), sy = yi < yf ? 1 : -1;
+	int err = (dx > dy ? dx : -dy) / 2, e2;
+
+	for (;;) {
+		draw_pixel(xi, yi, color);
+		if (xi == xf && yi == yf)
+			break;
+		e2 = err;
+		if (e2 > -dx) {
+			err -= dy;
+			xi += sx;
 		}
-		if(xi>xf)
-		{
-			SWAP(xi,xf);
-			SWAP(yi,yf);
+		if (e2 < dy) {
+			err += dx;
+			yi += sy;
 		}
-		if(yi>yf)
-			incry=-1;
-		else
-			incry=1;
-		d=2*dy-dx;
-		incre=2*dy;
-		incrne=2*(dy-dx);
-		while(xi<xf)
-		{
-			if(d<=0)
-				d+=incre;
-			else
-			{
-				d+=incrne;
-				yi+=incry;
+	}
+
+	int ipc_status;
+	message msg;
+	int validation = 0;
+	int receive;
+
+	int irq_set = kbd_subscribe();
+
+	while (!validation) {
+		receive = driver_receive(ANY, &msg, &ipc_status);
+		if (receive != 0) {
+			printf("driver_receive failed with: %d", receive);
+			continue;
+		}
+
+		if (is_ipc_notify(ipc_status)) {
+			switch (_ENDPOINT_P(msg.m_source)) {
+			case HARDWARE:
+				if (msg.NOTIFY_ARG & irq_set) {
+					validation = kbd_handler_c();
+				}
+				break;
+			default:
+				break;
 			}
-			xi++;
-			if(slopegt1)
-				draw_pixel(yi,xi,color);
-			else
-				draw_pixel(xi,yi,color);
+		} else {
 		}
-		sleep(5);
+	}
+	kbd_unsubscribe();
 	vg_exit();
+	return 0;
 }
 
 int test_xpm(unsigned short xi, unsigned short yi, char *xpm[]) {
-	
-	/* To be completed */
-	
-}	
 
-int test_move(unsigned short xi, unsigned short yi, char *xpm[], 
-				unsigned short hor, short delta, unsigned short time) {
-	
 	/* To be completed */
-	
-}					
 
+}
+
+int test_move(unsigned short xi, unsigned short yi, char *xpm[],
+		unsigned short hor, short delta, unsigned short time) {
+
+	/* To be completed */
+
+}
 
