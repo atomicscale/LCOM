2c402d863d2e29d3e436da34f32f52a334da161d
up201207046 <up201207046@7d022d26-2eca-423b-9939-685a0713be1c>
2014-12-01 11:29:02 +0000

    update
    git-svn-id: https://svn.fe.up.pt/repos/lcom1415-t3g07@102 7d022d26-2eca-423b-9939-685a0713be1c

---
+++ proj/src/utilities.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "stdio.h"
+
+extern FILE* logfile;
+#define LOG_PATH "home/lcom/log.txt"
+#define LOG(tag, msg) fprintf(logfile, "%s: %s\n", tag, msg)
+#define LOG_VAR(tag, var) fprintf(logfile, "%s: %d\n", tag, var)
